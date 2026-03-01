9e399ca934647c9ed3f9a84f7f9c8dff49fedda5
Good luck Steven <goodlucksteven676@gmail.com>
2026-03-01 17:12:03 +0000

    Based on the build logs in the screenshot, your deployment for SmarterSe

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
